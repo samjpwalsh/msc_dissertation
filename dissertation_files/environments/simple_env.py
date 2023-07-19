889b0fe272e3e67460c085f01095a6bd986bf198
Sam Walsh <samjpwalsh@gmail.com>
2023-07-19 18:31:40 +0100

    refactoring and simple env set up

---
+++ dissertation_files/environments/simple_env.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+from minigrid.core.constants import COLOR_NAMES
+from minigrid.core.grid import Grid
+from minigrid.core.mission import MissionSpace
+from minigrid.core.world_object import Door, Goal, Key, Wall
+from minigrid.minigrid_env import MiniGridEnv
+
+
+class SimpleEnv(MiniGridEnv):
+    def __init__(
+            self,
+            size=10,
+            agent_start_pos=(1, 1),
+            agent_start_dir=0,
+            max_steps=500,
+            **kwargs,
+    ):
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+
+        mission_space = MissionSpace(mission_func=self._gen_mission)
+
+        super().__init__(
+            mission_space=mission_space,
+            grid_size=size,
+            see_through_walls=False,
+            max_steps=max_steps,
+            **kwargs,
+        )
+
+    @staticmethod
+    def _gen_mission():
+        return "simple env"
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Place a goal square in the bottom-right corner
+        self.put_obj(Goal(), width - 2, height - 2)
+
+        # Place the agent
+        self.agent_pos = self.agent_start_pos
+        self.agent_dir = self.agent_start_dir
+
+        self.mission = "simple env"
+
